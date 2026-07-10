93660a9c39d30afefcc501f2af286db7b413a8f2
aglcode <webc26696@gmail.com>
2026-07-10 11:16:01 +0800

    navigations

---
+++ app/onboarding.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const onboarding = () => {
+  return (
+    <View>
+      <Text>Hello user this is onboarding</Text>
+    </View>
+  );
+};
+
+export default onboarding;
